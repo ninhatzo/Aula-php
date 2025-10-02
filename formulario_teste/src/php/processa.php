43ecb4858e940eec410408e5e2a7527c4e639f51
ninhatzo <anac.estudantes2@gmail.com>
2025-10-01 21:02:39 -0300

    Criando o formulário de teste php

---
+++ formulario_teste/src/php/processa.php
@@ -0,0 +1,15 @@
+<?php
+
+// $_POST -> É uma super global, que armazena dados de forma automática 
+// Armazena dados vindo de uma requisição HTTP.
+// No caso de $_POST, ele contém dados enviados via método POST em um formulário HTML
+// ['nome'] -> É o índice(ou chave) usado para acessar um elemento específico do array associativo.
+// Neste exemplo, nome é um atributo "name" do campo do formulário HTML.
+    $nome = $POST['nome'];
+    $email = $POST['email'];
+
+    echo "<h2> Dados recebidos:</h2>";
+    echo "Nome: ". htmlspecialchars($nome)."<br>";
+    echo "E-mail: ". htmlspecialchars($email)."<br>";
+
+?>
