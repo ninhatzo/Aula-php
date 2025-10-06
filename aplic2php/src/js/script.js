3f61de23836c6a532db637182951efbee713c4c5
ninhatzo <anac.estudantes2@gmail.com>
2025-10-06 20:16:03 -0300

    Colocando focus no js

--- aplic2php/src/js/script.js
+++ aplic2php/src/js/script.js
@@ -1,13 +1,19 @@
 function validaFormulario() {
 
-        let nome = document.getElementById("nome").value.trim();
-        let email = document.getElementById("email").value.trim();
-    
-        if(nome==="" || email==="") {
-            
-            alert("Preencher todos os campos!");
+        let camponome = document.getElementById("nome");
+        let nome = camponome.value.trim();
+
+        let campoemail = document.getElementById("email");
+        let email = campoemail.value.trim();
+
+        if(nome==="") {
+            alert("Preencher o campo nome!");
+            camponome.focus();
+            return false;
+        } else if(email==="") {
+            alert("Preencha o campo E-mail!");
+            campoemail.focus();
             return false;
         }
-    
         return true;
     }
--- aplic2php/src/php/processa.php
+++ aplic2php/src/php/processa.php
@@ -14,16 +14,15 @@
         //Verificar se o campo está vazio
         if(empty($nome)) {
 
-            echo "O Campo nome está vazio.<br>";
+            echo "<div class='caixa'>O Campo nome está vazio.</div><br>";
             //echo "<br><a href='../../index.html'>Voltar</a>";
             sair(); //Chamando a função sair
         } else if(empty($email)) {
             
-            echo "O campo E-mail está vazio.<br>";
+            echo "<div class='caixa'>O campo E-mail está vazio.</div><br>";
             //echo "<br><a href='../../index.html'>Voltar</a>";
             sair(); //Chamando a função sair
         } else if(!empty($nome)&& !empty($email)) {
-
             echo "Nome: ". htmlspecialchars($nome). "<br>";
             echo "E-mail: ". htmlspecialchars($email). "<br>";
             //echo "<br><a href='../../index.html'>Voltar</a>";
@@ -31,3 +30,78 @@
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados Recebidos</title>
+    <style>
+        
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: Arial, sans-serif;
+    background-color: rgb(239, 241, 230);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.caixa {
+    position: fixed;
+    width: 800px;
+    text-transform: uppercase;
+    font-size: 40px;
+    margin-bottom: 15px;
+    color: rgb(176, 192, 139);
+    font-weight: bold;
+    text-align: center;
+    justify-content: center;
+    border-radius: 50px;
+    padding: 50px;
+    background-color: #ffffff;
+    border: 1px solid #ffffff;
+    box-shadow: 0 0 3px rgb(221, 223, 207);
+    z-index: 1;
+}
+
+.title {
+    text-transform: uppercase;
+    font-size: 20px;
+    margin-bottom: 15px;
+    color: rgb(108, 141, 148);
+}
+
+a {
+    position: fixed;
+    text-decoration: none;
+    width: 5%;
+    margin-top: -90vh;
+    margin-left: -205vh;
+    text-align: center;
+    padding: 10px;
+    font-size: 20px;
+    cursor: pointer;
+    border-radius: 100px;
+    color: #ffffff;
+    background-color: rgb(204, 212, 167);
+    border: 1px solid rgb(204, 212, 167);
+}
+
+a:hover {
+    background-color: rgb(171, 182, 122);
+    border: 1px solid rgb(171, 182, 122);
+    color: #ffffff;
+} 
+    </style>
+</head>
+<body>
+</body>
+</html>
