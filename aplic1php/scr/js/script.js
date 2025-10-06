40dbdc25b7a2a152573d34eaad61b2847ea46749
ninhatzo <anac.estudantes2@gmail.com>
2025-10-06 20:11:43 -0300

    Colocando focus no campo

--- aplic1php/scr/js/script.js
+++ aplic1php/scr/js/script.js
@@ -2,14 +2,26 @@ function validaFormulario() {
 
 // A função trim() serve para remover possíveis espaços em branco no inicio ou no final do input do campo.
 
-    let nome = document.getElementById("nome").value.trim();
-    let email = document.getElementById("email").value.trim();
-    let mensagem = document.getElementById("mensagem").value.trim();
+    let camponome = document.getElementById("nome");
+    let nome = camponome.value.trim();
 
-    if(nome==="" || email==="" || mensagem==="") {
-        
-        alert("Preencher todos os campos!");
-        
+    let campoemail = document.getElementById("email");
+    let email = campoemail.value.trim();
+
+    let campomensagem = document.getElementById("mensagem");
+    let mensagem = campomensagem.value.trim();
+
+    if(nome==="") {
+        alert("Preencher o campo nome!");
+        camponome.focus();
+        return false;
+    } else if(email==="") {
+        alert("Preencha o campo E-mail!");
+        campoemail.focus();
+        return false;
+    } else if (mensagem==="") {
+        alert("Preencha o campo de mensagem!");
+        campomensagem.focus();
         return false;
     }
 
